0e43d59e94ba501839f9dd153104e4e0bb73a5c2
Fernando Alexis <vale.alexis18@gmail.com>
2024-08-12 23:28:21 -0300

    se obtuvieron todas las fechas correspondiente al año 2010 y se los guardo en un csv

---
+++ Temas-Vistos/prueba.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd 
+
+def get_dates(year):
+  dates = pd.date_range(start = f'{year}-01-01', end = f'{year}-12-31')
+  dates_str = dates.strftime('%d-%m-%Y').to_numpy()
+  return dates_str
+def save_dates(dates,year_save):
+  year_save = str(year_save)
+  df = pd.DataFrame(dates,columns=['Date'])
+  name_csv ='dates'+ year_save + '.csv'
+  df.to_csv('dataset/'+name_csv,index=False)
+  
+  return 0
+data = save_dates(get_dates(2011),2011)
+
+
+
